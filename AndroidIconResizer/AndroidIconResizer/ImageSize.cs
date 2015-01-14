2894b1654114cc799a6e71e2bace253501f038d0
Josh <josh.quintus@live.com>
2015-01-14 15:47:15 -0500

    Ignore packages.  Forgot to include one code file.

---
+++ AndroidIconResizer/AndroidIconResizer/ImageSize.cs
@@ -0,0 +1,28 @@
+﻿namespace AndroidIconResizer
+{
+    public class ImageSize
+    {
+        public ImageSize(string name, double ratio)
+        {
+            Name = name;
+            Ratio = ratio;
+        }
+
+        public static ImageSize[] Sizes
+        {
+            get
+            {
+                return new ImageSize[]{
+                  new ImageSize("drawable-ldpi", .75),
+                  new ImageSize("drawable-mdpi", 1),
+                  new ImageSize("drawable-hdpi", 1.5),
+                  new ImageSize("drawable-xhdpi",    2),
+                };
+            }
+        }
+
+        public string Name { get; private set; }
+
+        public double Ratio { get; private set; }
+    }
+}
