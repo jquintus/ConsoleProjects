ff26cfbdb03b02b3580ebad7708b54c43eadf8f2
jquintus <josh.quintus@live.com>
2015-03-06 16:46:04 -0500

    read paths from command line.

--- PubProfileToFilezilla/PubProfileToFilezilla/Program.cs
+++ PubProfileToFilezilla/PubProfileToFilezilla/Program.cs
@@ -8,16 +8,59 @@ namespace PubProfileToFilezilla
 {
     public class Program
     {
+        public const string USAGE = @"
+USAGE
+    pubToFz path_to_input [path_to_output]
+
+If path_to_output is omitted, the file is saved as filezilla.xml in the current directory
+
+Example
+    pubToFz c:\users\myName\downloads\file.publishProfile
+
+
+";
         public static void Main(string[] args)
         {
+            try
+            {
+            string input = GetInput(args);
+            string output = GetOutput(args);
 
+            if (input == null || output == null)
+            {
+                Console.WriteLine(USAGE);
+                return;
+            }
 
-            var profile = new ProfileReader().Read("SampleInput.xml");
-            var zilla = Converter.Convert(profile);
+                var profile = new ProfileReader().Read(input);
+                var zilla = Converter.Convert(profile);
 
-            new FileZillaWriter().Write(zilla, "output.xml");
+                new FileZillaWriter().Write(zilla, output);
 
-            Console.WriteLine("Done");
+                Console.WriteLine("completed successfully");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was an error processing the files");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static string GetOutput(string[] args)
+        {
+            return GetArg(args, 1) ?? "filezilla.xml";
+        }
+
+        private static string GetInput(string[] args)
+        {
+            return GetArg(args, 0);
+        }
+
+        private static string GetArg(string[] args, int position)
+        {
+            if (args == null || args.Length <= position) return null;
+
+            return args[position];
         }
     }
 }
--- PubProfileToFilezilla/readme.md
+++ PubProfileToFilezilla/readme.md
@@ -1,11 +1,11 @@
 Console app to parse FTP data out of [Microsoft Azure publish profiles](https://msdn.microsoft.com/en-us/library/dn385850%28v=nav.70%29.aspx) and save it in a format that can be imported by [FileZilla Client](https://filezilla-project.org/)
-	
+    
 ## Command Line Arguments ##
 
 
     pubToFz path_to_input [path_to_output]
     
-If path_to_output is ommitted, the file is saved as filezilla.xml in the current directory     
+If path_to_output is omitted, the file is saved as filezilla.xml in the current directory     
 
 
     
